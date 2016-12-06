c6b04592211c613b4682d85d3a038853a79750f7
Wagner <stwagne2@illinois.edu>
2016-12-05 22:22:33 -0600

    Lagrange

--- project_math.m
+++ project_math.m
@@ -17,10 +17,13 @@ syms t3;
 syms t1_dot;
 syms t2_dot;
 syms t3_dot;
-syms a1;
-syms a2;
-syms a3;
+syms t;
+a1 = 1;
+a2 =1;
+a3 = 1;
 g = 9.81;
+Tau = [0,0,0];
+
 
 A_1 = [ cos(t1), -sin(t1), 0, a1 * cos(t1); ...
         sin(t1), cos(t1), 0, a1 * sin(t1); ...      
@@ -37,7 +40,7 @@ A_3 = [ cos(t3), -sin(t3), 0, a3 * cos(t3); ...
 T_02 = simplify(A_1* A_2)
 T_03 = simplify(T_02 * A_3)
 z = [0; 0; 1];
-O
+
 
 x = [1;1;1;];
 y = [1;1;1;];
@@ -81,8 +84,8 @@ Jv(2,:,:) = [diff(x2,t1),diff(x2,t2),diff(x2,t3);...
     diff(z2,t1),diff(z2,t2),diff(z2,t3)];
 
 x3 = a1*cos(t1) +a2*cos(t1+t2) +a3*cos(t1+t2+t3)/2;
-y2 = a1*sin(t1) +a2*sin(t1+t2) +a3*sin(t1+t2+t3)/2;
-z2 = 0;
+y3 = a1*sin(t1) +a2*sin(t1+t2) +a3*sin(t1+t2+t3)/2;
+z3 = 0;
 
 Jv(3,:,:) = [diff(x3,t1),diff(x3,t2),diff(x3,t3);...
     diff(y3,t1),diff(y3,t2),diff(y3,t3);...
@@ -93,6 +96,7 @@ Jv(3,:,:) = [diff(x3,t1),diff(x3,t2),diff(x3,t3);...
 U = m(1)*g*y1 + m(2)*g*y2 +m(2)*g*y3;
 
 q_dot = [t1_dot;t2_dot;t3_dot];
+q = [t1;t2;t3];
 
 T = m(1).*q_dot'.*Jv(1,:,:)'.*Jv(1,:,:).*q_dot +...
     q_dot'.*Jw(1,:,:)'.*I(1,:,:).*Jw(1,:,:).*q_dot +...
@@ -100,5 +104,12 @@ T = m(1).*q_dot'.*Jv(1,:,:)'.*Jv(1,:,:).*q_dot +...
     q_dot'.*Jw(2,:,:)'.*I(2,:,:).*Jw(2,:,:).*q_dot +...
     m(3).*q_dot'.*Jv(3,:,:)'.*Jv(3,:,:).*q_dot +...
     q_dot'.*Jw(3,:,:)'.*I(3,:,:).*Jw(3,:,:).*q_dot; 
+
+%Lagrangian
+L = T-U;
+
+Tau = diff(diff(L,q_dot),t) - diff(L,q);
+
+
 
 
